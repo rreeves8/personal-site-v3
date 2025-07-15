38fc58c8ad9f8b35bd3558f865205378dea092ff
Magnus Reeves <rreeves8@uwo.ca>
2025-07-15 10:33:53 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Magnus Reeves",
   description: "Personal site",
+  icons: {
+    icon: "https://gravatar.com/avatar/65a2b04d5bf09d66ab59ca5a1b3c52ee253530e87ee477f78f8b7779bd8814c9",
+  },
 };
 
 export default function RootLayout({
